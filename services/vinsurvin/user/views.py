9c2d1c85b166ca7f1ea149e46896bcb7eed8e419
Firstname Lastname <alexis.etheve@etu.univ-grenoble-alpes.fr>
2024-09-09 18:48:05 +0200

    link frontend login form to backend

--- services/vinsurvin/user/views.py
+++ services/vinsurvin/user/views.py
@@ -25,3 +25,33 @@ def create_user(request):
             return JsonResponse({'error': str(e)}, status=500)
     else:
         return JsonResponse({'error': 'Méthode non autorisée'}, status=405)
+    
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            username = data.get('email')
+            password = data.get('password')
+
+            if not all([username, password]):
+                return JsonResponse({'error': 'Tous les champs sont requis'})
+            
+            user = User.objects.get(username=username)
+
+            if not user.check_password(password):
+                return JsonResponse({'error': 'Mot de passe incorrect'}, status=400)
+
+            user_data = {
+                'date_joined': user.date_joined,
+            }
+            
+            return JsonResponse({'message': 'Connexion réussie', 'user': user_data})
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Données JSON invalides'}, status=400)
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'Utilisateur non trouvé'}, status=404)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    else:
+        return JsonResponse({'error': 'Méthode non autorisée'}, status=405)
