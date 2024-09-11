3827cd505744c5e756ae9d47d5ecc0f80e506d0a
lachappp <chapurlat.adrien@gmail.com>
2024-09-11 09:56:28 +0200

    add fields to Product

--- services/vinsurvin/products/models.py
+++ services/vinsurvin/products/models.py
@@ -6,16 +6,15 @@ class Product(djongo_models.Model):
     name = models.CharField(max_length=50, null=True)
     type = models.CharField(max_length=50, null=True)
     image = models.CharField(max_length=255, null=True)    
-    millesime = models.IntegerField(blank=True, null=True)
     stock = models.IntegerField(null=True)
     price = models.FloatField(null=True)
+    millesime = models.IntegerField(blank=True, null=True)
     promo = models.FloatField(blank=True, null=True)
-    description = models.TextField(null=True)
+    description = models.TextField(blank=True, null=True)
     producer = models.CharField(max_length=50, blank=True, null=True)
-    grape_variety = models.CharField(max_length=50, null=True)
-    region = models.CharField(max_length=50, null=True)
-    appellation = models.CharField(max_length=50, null=True)
-    
+    grape_variety = models.CharField(max_length=50, blank=True, null=True)
+    region = models.CharField(max_length=50, blank=True, null=True)
+    appellation = models.CharField(max_length=50, blank=True, null=True)
     
 
     objects = djongo_models.DjongoManager()
