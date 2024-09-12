8e5e72e5373983099cb59f2a7ad220e1cea2ff51
lachappp <chapurlat.adrien@gmail.com>
2024-09-12 10:18:57 +0200

    add search product by numero

--- services/vinsurvin/product/models.py
+++ services/vinsurvin/product/models.py
@@ -3,19 +3,20 @@ from djongo import models as djongo_models
 from django.core.exceptions import ValidationError
 
 class Product(djongo_models.Model):
+    numero = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, null=True)
-    type = models.CharField(max_length=50, null=True)
-    image = models.CharField(max_length=255, null=True)    
-    stock = models.IntegerField(null=True)
     price = models.FloatField(null=True)
-    millesime = models.IntegerField(blank=True, null=True)
     promo = models.FloatField(blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(null=True)
+    image = models.CharField(max_length=255, null=True)
+    stock = models.IntegerField(null=True)
+    type = models.CharField(max_length=50, null=True)
+    domain = models.CharField(max_length=50, blank=True, null=True)
     producer = models.CharField(max_length=50, blank=True, null=True)
-    grape_variety = models.CharField(max_length=255, blank=True, null=True)
+    millesime = models.IntegerField(blank=True, null=True)
+    grape_variety = models.CharField(max_length=50, blank=True, null=True)
     region = models.CharField(max_length=50, blank=True, null=True)
-    appelation = models.CharField(max_length=50, blank=True, null=True)
-    
+    appellation = models.CharField(max_length=50, blank=True, null=True)
 
     objects = djongo_models.DjongoManager()
 
