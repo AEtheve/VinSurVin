ad8a762111b04524b8e00a597331ae09846030cf
lachappp <chapurlat.adrien@gmail.com>
2024-09-12 16:46:27 +0200

    add cart to user and method add_to_cart delete and get

--- services/vinsurvin/user/models.py
+++ services/vinsurvin/user/models.py
@@ -1,5 +1,33 @@
 from django.db import models
-from db_connection import db
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from djongo import models as djongo_models
 
-user_collection = db.user
+class User(AbstractUser):
+    cart = djongo_models.JSONField(default=dict, null=True, blank=True)
+
+    def add_to_cart(self, product_id, quantity):
+        if self.cart is None:
+            self.cart = []
+        
+        item = next((item for item in self.cart if item['product_id'] == product_id), None)
+        if item:
+            item['quantity'] += quantity
+        else:
+            self.cart.append({'product_id': product_id, 'quantity': quantity})
+        self.save()
+
+    def remove_from_cart(self, product_id, quantity=1):
+        if self.cart is None:
+            return False
+        item = next((item for item in self.cart if item['product_id'] == product_id), None)
+        if item:
+            if item['quantity'] > quantity:
+                item['quantity'] -= quantity
+            else:
+                self.cart.remove(item)
+            self.save()
+            return True
+        return False
+        
+    def get_cart(self):
+        return self.cart
