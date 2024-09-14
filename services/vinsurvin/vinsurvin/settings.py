7cfe0d451f0186052058752f3f4ebe63973c70d3
Firstname Lastname <alexis.etheve@etu.univ-grenoble-alpes.fr>
2024-09-14 09:02:07 +0200

    add containers

--- services/vinsurvin/vinsurvin/settings.py
+++ services/vinsurvin/vinsurvin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,6 +91,13 @@ WSGI_APPLICATION = 'vinsurvin.wsgi.application'
 # }
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': os.environ.get('DB_NAME', 'default_db_name'),
+        'CLIENT': {
+            'host': os.environ.get('DB_HOST', 'localhost'),
+        }  
+    }
 }
 
 # Password validation
