2fefb4356e4737bb83111b26a389581eeab00b4b
lachappp <chapurlat.adrien@gmail.com>
2024-09-18 15:15:11 +0200

    add email to guest user

--- services/vinsurvin/user/views.py
+++ services/vinsurvin/user/views.py
@@ -138,15 +138,16 @@ def create_order(request):
         street = data.get('street')
         city = data.get('city')
         zip_code = data.get('zip_code')
-        if not is_user_logged(request):
-            email = data.get('email')
-            if not email:
-                return JsonResponse({'error': 'Email is required'}, status=400)
-
         if not all([street, city, zip_code]):
             return JsonResponse({'error': 'Address is required'}, status=400)
+        
+        email = None
+        if user.is_anonymous_user:
+            email = data.get('email')
+            if not email:
+                return JsonResponse({'error': 'Email is required for anonymous users'}, status=400)
 
-        order = user.create_order(street, city, zip_code)
+        order = user.create_order(street, city, zip_code, email=email)
         return JsonResponse({'message': 'Order created successfully', 'order': order}, status=201)
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
